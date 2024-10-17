eb6366d4fe132ee9da9e00a09840e50ea7457fbd
Rume7 <electengrhume@gmail.com>
2024-10-17 10:50:52 +0100

    Added JavaDoc

--- src/main/java/com/codehacks/blog/BlogApplication.java
+++ src/main/java/com/codehacks/blog/BlogApplication.java
@@ -3,9 +3,13 @@ package com.codehacks.blog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * A Simple blog application.
+ */
 @SpringBootApplication
 public class BlogApplication {
-    public static void main(String[] args) {
+
+    public static void main(final String[] args) {
         SpringApplication.run(BlogApplication.class, args);
     }
 }
