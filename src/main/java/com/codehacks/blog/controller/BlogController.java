8b7e0b6c797227eae88f8afef2910731d34ad54d
Rume7 <electengrhume@gmail.com>
2024-10-17 11:56:08 +0100

    Fixed imports

--- src/main/java/com/codehacks/blog/controller/BlogController.java
+++ src/main/java/com/codehacks/blog/controller/BlogController.java
@@ -4,7 +4,11 @@ import com.codehacks.blog.model.Post;
 import com.codehacks.blog.service.BlogServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
