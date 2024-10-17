d3b9e08a82094c0403c63a6346f1d7289bf7ffe2
Rume7 <electengrhume@gmail.com>
2024-10-17 11:44:58 +0100

    Added file

--- src/main/java/com/codehacks/blog/controller/BlogController.java
+++ src/main/java/com/codehacks/blog/controller/BlogController.java
@@ -1,33 +1,36 @@
 package com.codehacks.blog.controller;
 
 import com.codehacks.blog.model.Post;
-import com.codehacks.blog.service.BlogService;
+import com.codehacks.blog.service.BlogServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * A controller for the blog application.
+ */
 @RestController
 @RequestMapping("/api/posts")
 public class BlogController {
 
-    @Autowired
-    private final BlogService blogService;
+    private final BlogServiceImpl blogServiceImpl;
 
-    public BlogController(BlogService blogService) {
-        this.blogService = blogService;
+    @Autowired
+    public BlogController(final BlogServiceImpl blogServiceImpl) {
+        this.blogServiceImpl = blogServiceImpl;
     }
 
     @GetMapping
     public ResponseEntity<List<Post>> getPosts() {
-        List<Post> posts = blogService.getPosts();
+        List<Post> posts = blogServiceImpl.getPosts();
         return ResponseEntity.ok(posts);
     }
 
     @PostMapping
     public ResponseEntity<Post> createPost(@RequestBody Post post) {
-        Post createdPost = blogService.createPost(post);
+        Post createdPost = blogServiceImpl.createPost(post);
         if (createdPost == null) {
             return ResponseEntity.notFound().build();
         }
