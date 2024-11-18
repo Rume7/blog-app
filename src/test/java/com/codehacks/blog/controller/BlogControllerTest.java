5f5e3b32a93060637a84632a714d63459dff0cc1
Rume7 <electengrhume@gmail.com>
2024-11-18 02:33:50 +0000

    Unit test for creating a blog post.

--- src/test/java/com/codehacks/blog/controller/BlogControllerTest.java
+++ src/test/java/com/codehacks/blog/controller/BlogControllerTest.java
@@ -1,23 +1,41 @@
 package com.codehacks.blog.controller;
 
 import com.codehacks.blog.config.SecurityConfig;
+import com.codehacks.blog.exception.InvalidPostException;
+import com.codehacks.blog.model.Author;
+import com.codehacks.blog.model.CustomUserDetails;
+import com.codehacks.blog.model.Post;
+import com.codehacks.blog.model.Role;
 import com.codehacks.blog.service.BlogService;
 import com.codehacks.blog.service.TokenService;
+import com.codehacks.blog.util.Constants;
 import com.codehacks.blog.util.JwtUtil;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
-import org.mockito.Mock;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.Collections;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @EnableAspectJAutoProxy
 @WebMvcTest(BlogController.class)
@@ -34,6 +52,9 @@ class BlogControllerTest {
     private TokenService tokenService;
 
     @MockBean
+    private BlogService blogService;
+
+    @MockBean
     private JwtUtil jwtUtil;
 
     private final ObjectMapper objectMapper = new ObjectMapper();
@@ -46,4 +67,30 @@ class BlogControllerTest {
                 .build();
     }
 
+    @Test
+    void testCreatePostWithValue() throws InvalidPostException, Exception {
+        String username = "testUser";
+        UserDetails userDetails = createUserDetails(username, Role.SUBSCRIBER);
+
+        Post post = new Post("Blog Title", "This is the content of the post");
+        Author author = new Author("Larry", "Fink");
+        post.setAuthor(author);
+
+        when(blogService.createPost(any(Post.class))).thenReturn(post);
+
+        ResultActions resultActions = mockMvc.perform(post(Constants.BLOG_PATH + "/create")
+                        .with(user(userDetails))
+                        .param("username", username)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(post)))
+                .andDo(print());
+
+        resultActions.andExpect(status().isOk());
+        verify(blogService, times(1)).createPost(post);
+    }
+
+    private UserDetails createUserDetails(String username, Role role) {
+        return new CustomUserDetails(username, "Register123Password", "user@example.com",
+                role, Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role.name())), true);
+    }
 }
