7b1818e3cbce04238375a0175d05442994b22c3a
Rume7 <electengrhume@gmail.com>
2025-03-22 14:59:55 +0000

    Fixed MailConfig test and prop file.

--- src/main/java/com/codehacks/blog/config/MailConfig.java
+++ src/main/java/com/codehacks/blog/config/MailConfig.java
@@ -3,26 +3,24 @@ package com.codehacks.blog.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
 @Configuration
-@Primary
 public class MailConfig {
 
-    @Value("${MAIL_HOST_1:localhost}")
+    @Value("${spring.mail.host:smtp.gmail.com}")
     private String mailHost;
 
-    @Value("${MAIL_PORT_1:25}")
+    @Value("${spring.mail.port:587}")
     private int mailPort;
 
-    @Value("${MAIL_USERNAME_1}")
+    @Value("${spring.mail.username:example@gmail.com}")
     private String mailUsername;
 
-    @Value("${MAIL_PASSWORD_1}")
+    @Value("${spring.mail.password:}")
     private String mailPassword;
 
     @Bean
@@ -31,13 +29,18 @@ public class MailConfig {
         mailSender.setHost(mailHost);
         mailSender.setPort(mailPort);
         mailSender.setUsername(mailUsername);
+
+        if (mailPassword == null || mailPassword.isEmpty()) {
+            throw new IllegalStateException("Mail password is not set! Please configure 'spring.mail.password'.");
+        }
         mailSender.setPassword(mailPassword);
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.debug", "true");
+        props.put("mail.debug", "false");                   // Set to "true" only for debugging
+        props.put("mail.smtp.ssl.trust", "smtp.gmail.com"); // Avoid SSL handshake issues
 
         return mailSender;
     }
